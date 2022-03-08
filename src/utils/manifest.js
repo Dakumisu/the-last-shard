6d779d4c6cb0bf24b4e0d4c11e9c4024735a6926
Titouan Pellerin <titouan.pellerin@gmail.com>
2022-03-08 16:52:27 +0100

    added manifest

---
+++ src/utils/manifest.js
@@ -0,0 +1,10 @@
+// Assets Maps
+const imagesMap = new Map();
+const soundsMap = new Map();
+const modelsMap = new Map();
+const videosMap = new Map();
+
+// Fill Maps
+imagesMap.set('favicon', { path: '/favicon.png', data: {} });
+
+export { imagesMap, soundsMap, modelsMap, videosMap };
