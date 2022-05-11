f73e8b0cc4d1086bba66af7de32d3118358d0f04
Alex <alex.gattefosse@gmail.com>
2022-05-11 18:00:18 +0200

    package update

--- src/js/Webgl/Materials/Grass/vertex.glsl
+++ src/js/Webgl/Materials/Grass/vertex.glsl
@@ -1,4 +1,6 @@
 #pragma glslify: cnoise = require('philbin-packages/glsl/noises/classic/2d')
+#pragma glslify: smoothNoise = require('philbin-packages/glsl/noises/smooth/2d')
+#pragma glslify: map = require('philbin-packages/glsl/maths/map')
 
 uniform float uTime;
 uniform float uWindSpeed;
@@ -17,31 +19,6 @@ attribute vec3 aPositions;
 varying vec3 vPos;
 varying float vFade;
 varying float vNoiseMouvement;
-
-float N(vec2 st) {
-	return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
-}
-
-float smoothNoise(vec2 ip) {
-	vec2 lv = fract(ip);
-	vec2 id = floor(ip);
-
-	lv = lv * lv * (3. - 2. * lv);
-
-	float bl = N(id);
-	float br = N(id + vec2(1, 0));
-	float b = mix(bl, br, lv.x);
-
-	float tl = N(id + vec2(0, 1));
-	float tr = N(id + vec2(1, 1));
-	float t = mix(tl, tr, lv.x);
-
-	return mix(b, t, lv.y);
-}
-
-float map(float value, float start1, float stop1, float start2, float stop2) {
-	return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
-}
 
 void main() {
 	float boxSize = uHalfBoxSize * 2.;
